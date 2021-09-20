8c3c7d576cc04b168b2c01e6b7fd0b3e570f41d9
Wakky <wakky1free@gmail.com>
2021-09-20 10:21:14 +0900

    Update zybo_top.v
    fix the bit length of LED_COUNTER_1SEC

--- rtl/zybo_top.v
+++ rtl/zybo_top.v
@@ -6,17 +6,17 @@ output reg [3:0]  LED
 
 );
 
-parameter LED_COUNTER_1SEC = 26'd124999999;
+parameter LED_COUNTER_1SEC = 27'd124999999;
 
-reg [25:0]  counter_1sec;
+reg [26:0]  counter_1sec;
 
 always @ (posedge CLK) begin
   if (RST)
-    counter_1sec <= 26'd0;
+    counter_1sec <= 27'd0;
   else if (counter_1sec == LED_COUNTER_1SEC)
-    counter_1sec <= 26'd0;
+    counter_1sec <= 27'd0;
   else
-    counter_1sec <= counter_1sec + 26'd1;
+    counter_1sec <= counter_1sec + 27'd1;
 end
 
 always @ (posedge CLK) begin
